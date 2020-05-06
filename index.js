bfd425d69c9dab7a374803ea587630d6674eed4f
Qasim Laghari <go.jets.laghari@gmail.com>
2020-05-05 18:58:40 -0700

    Setup get and post routes

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
 
 const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
 
 
 app.get('/posts', (req,res) => {
-
+    res.send(posts);
 });
 
 app.post('/posts', (req,res) => {
+    let id = randomBytes(4).toString('hex');
+    let {title} = req.body;
+
+    posts[id] = {
+        id, title
+    };
+
+    res.status(201).send(posts[id]);
+
 
 });
 
