3ccebba084132b00ab1ecc8c0ceb7c7808554148
Qasim Laghari <go.jets.laghari@gmail.com>
2020-05-05 18:50:12 -0700

    Initialize Express app

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/posts', (req,res) => {
+
+});
+
+app.post('/posts', (req,res) => {
+
+});
+
+app.listen(4000, () => {
+    console.log('Listening on Port 4000');
+});
