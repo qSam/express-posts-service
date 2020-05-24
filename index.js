6f3a91c5c6cc1b29ede76fa7e2ff18c08fec7ba4
Qasim Laghari <go.jets.laghari@gmail.com>
2020-05-24 14:40:19 -0700

    Handle receiving event in posts service

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ app.post('/posts', async (req,res) => {
 
 });
 
+app.post('/events', (req,res) => {
+    console.log('Received event', req.body.type);
+    res.send ({});
+});
+
 app.listen(4000, () => {
     console.log('Listening on Port 4000');
 });
